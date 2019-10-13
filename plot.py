3bc298c0d6d4f1540ffcd0276010d926c012e2ab
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2019-10-13 19:10:38 +0200

    plot.py: Add startup-time plot

--- plot.py
+++ plot.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
 titles = {
-        "results/zplug-inst.txt" : "zplug install",
-        "results/zgen-inst.txt" : "zgen install",
-        "results/zplugin-light-inst.txt" : "zplugin-light install",
-        "results/zplugin-load-inst.txt" : "zplugin-load install",
-        "results/zplugin-turbo-inst.txt" : "zplugin Turbo install",
-        "results/zplug.txt" : "zplug startup",
-        "results/zgen.txt" : "zgen startup",
-        "results/zplugin-light.txt" : "zplugin-light startup",
-        "results/zplugin-load.txt" : "zplugin-load startup",
-        "results/zplugin-turbo.txt" : "zplugin Turbo startup",
+        "results/zplug-inst.txt" : "zplug",
+        "results/zgen-inst.txt" : "zgen",
+        "results/zplugin-light-inst.txt" : "zplugin light",
+        "results/zplugin-load-inst.txt" : "zplugin load",
+        "results/zplugin-turbo-inst.txt" : "zplugin (Turbo) load",
+
+        "results/zplug.txt" : "zplug",
+        "results/zgen.txt" : "zgen",
+        "results/zplugin-light.txt" : "zplugin light",
+        "results/zplugin-load.txt" : "zplugin load",
+        "results/zplugin-turbo.txt" : "zplugin (Turbo) load",
 }
 
 files_inst = [
@@ -32,6 +33,10 @@ files_startup = [
         "results/zplugin-turbo.txt",
 ]
 
+#
+# Installation-times plot
+#
+
 data={}
 for fname in files_inst:
     odd=0
@@ -46,6 +51,48 @@ for fname in files_inst:
             odd = 1 - odd
             if not odd:
                 fields = line.split()
+                idata.append( float(fields[ -2 ]) / 1000.0 )
+        else:
+            fields = line.split()
+            idata.append( float(fields[ -2 ]) / 1000.0 )
+
+    mean = np.mean(idata)
+    data[titles[fname]]=mean
+
+group_data = list(data.values())
+group_names = list(data.keys())
+
+fig, ax = plt.subplots(figsize=(8.2, 5))
+ax.bar(group_names, group_data)
+
+# Add a vertical line, here we set the style in the function call
+ax.axhline(mean, ls='--', color='r')
+
+# Now we'll move our title up since it's getting a little cramped
+ax.title.set(y=1.05)
+
+plt.title("Installation time, in seconds")
+
+plt.show()
+
+#
+# Startup-times plot
+#
+
+data={}
+for fname in files_startup:
+    odd=0
+
+    file = open( fname, "r" )
+    lines = file.readlines()
+    file.close()
+
+    idata=[]
+    for line in lines:
+        if fname == "results/zplug.txt":
+            odd = 1 - odd
+            if not odd:
+                fields = line.split()
                 idata.append( float(fields[ -2 ]) )
         else:
             fields = line.split()
@@ -57,7 +104,7 @@ for fname in files_inst:
 group_data = list(data.values())
 group_names = list(data.keys())
 
-fig, ax = plt.subplots(figsize=(5, 5))
+fig, ax = plt.subplots(figsize=(8.2, 5))
 ax.bar(group_names, group_data)
 
 # Add a vertical line, here we set the style in the function call
@@ -66,6 +113,6 @@ ax.axhline(mean, ls='--', color='r')
 # Now we'll move our title up since it's getting a little cramped
 ax.title.set(y=1.05)
 
-plt.title(titles[fname])
+plt.title("Startup time, in milliseconds")
 
 plt.show()
